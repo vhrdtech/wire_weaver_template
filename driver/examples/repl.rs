651a2b2ca6279ab0e3683de91e488e882748575d
Roman Isaikin <romix.lab@gmail.com>
2025-11-25 14:31:55 +0100

    refactor: improve REPL

--- driver/examples/repl.rs
+++ driver/examples/repl.rs
@@ -7,7 +7,6 @@ use console::style;
 use tracing::{error, info};
 
 #[derive(Parser)]
-#[command(name = "")] // This name will show up in clap's error messages, so it is important to set it to "".
 enum Command {
     Connect,
     Disconnect,
@@ -15,20 +14,119 @@ enum Command {
 
     LedOn,
     LedOff,
+    // Add your commands here.
+    // Arguments can be added like so: MyCommand { x: f32 },
+}
+
+async fn handle_command(driver: &mut MyDeviceDriver, cmd: Command) -> Result<()> {
+    match cmd {
+        Command::LedOn => {
+            driver.root()
+                .set_led_state(LedState::On)
+                .await?;
+        }
+        Command::LedOff => {
+            driver.root()
+                .set_led_state(LedState::Off)
+                .await?;
+        }
+        // Handle additional commands here
+
+        // Already handled
+        Command::Connect | Command::Disconnect | Command::Exit => {
+        }
+    }
+    Ok(())
+}
+
+async fn connect_to_device() -> Result<MyDeviceDriver> {
+    let filter = DeviceFilter::UsbVidPid {
+        vid: 0xc0de,
+        pid: 0xcafe,
+    };
+    let device = MyDeviceDriver::connect(filter, OnError::ExitImmediately).await?;
+    Ok(device)
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
-    // let cli = Cli::parse();
-
     let mut driver = None;
+    let mut rl = setup_repl();
 
+    loop {
+        let should_exit = handle_user_command(&mut driver, &mut rl).await?;
+        if should_exit {
+            break;
+        }
+    }
+
+    if let Some(d) = driver.as_mut() {
+        d.disconnect_and_exit().await?;
+    }
+    Ok(())
+}
+
+async fn handle_user_command(driver: &mut Option<MyDeviceDriver>, rl: &mut ClapEditor<Command>) -> Result<bool> {
+    match rl.read_command() {
+        ReadCommandOutput::Command(c) => match c {
+            Command::Connect => {
+                match connect_to_device().await {
+                    Ok(d) => {
+                        info!("Connected!");
+                        *driver = Some(d);
+                    }
+                    Err(e) => {
+                        error!("{}", e);
+                    }
+                }
+            }
+            Command::Disconnect => {
+                if let Some(mut d) = driver.take() {
+                    d.disconnect_and_exit().await?;
+                    info!("Disconnected!");
+                } else {
+                    info!("Already disconnected");
+                }
+            }
+            Command::Exit => return Ok(false),
+            c => {
+                let Some(d) = driver.as_mut() else {
+                    println!("{}", style("No connection, connect first").yellow());
+                    return Ok(true);
+                };
+                let r = handle_command(d, c).await;
+                match r {
+                    Ok(_) => println!("{}", style("ok").green()),
+                    Err(e) => println!("{}", style(e).red()),
+                }
+            }
+        },
+        ReadCommandOutput::EmptyLine => (),
+        ReadCommandOutput::ClapError(e) => {
+            e.print()?;
+        }
+        ReadCommandOutput::ShlexError => {
+            println!(
+                "{} input was not valid and could not be processed",
+                style("Error:").red().bold()
+            );
+        }
+        ReadCommandOutput::ReedlineError(e) => {
+            panic!("{e}");
+        }
+        ReadCommandOutput::CtrlC => return Ok(true),
+        ReadCommandOutput::CtrlD => return Ok(false),
+    }
+    Ok(false)
+}
+
+fn setup_repl() -> ClapEditor<Command> {
     let prompt = DefaultPrompt {
         left_prompt: DefaultPromptSegment::Basic("ww_template".to_owned()),
         ..DefaultPrompt::default()
     };
-    let mut rl = ClapEditor::<Command>::builder()
+    let rl = ClapEditor::<Command>::builder()
         .with_prompt(Box::new(prompt))
         .with_editor_hook(|reed| {
             // Do custom things with `Reedline` instance here
@@ -37,75 +135,5 @@ async fn main() -> Result<()> {
             ))
         })
         .build();
-    loop {
-        match rl.read_command() {
-            ReadCommandOutput::Command(c) => match c {
-                Command::Connect => {
-                    let filter = DeviceFilter::UsbVidPid {
-                        vid: 0xc0de,
-                        pid: 0xcafe,
-                    };
-                    match MyDeviceDriver::connect(filter, OnError::ExitImmediately).await {
-                        Ok(d) => {
-                            info!("Connected!");
-                            driver = Some(d);
-                        }
-                        Err(e) => {
-                            error!("{}", e);
-                        }
-                    }
-                }
-                Command::Disconnect => {
-                    if let Some(mut d) = driver.take() {
-                        d.disconnect_and_exit().await?;
-                        info!("Disconnected!");
-                    } else {
-                        info!("Already disconnected");
-                    }
-                }
-                Command::Exit => break,
-                c => {
-                    let Some(d) = driver.as_mut() else {
-                        println!("{}", style("No connection, connect first").yellow());
-                        continue;
-                    };
-                    let r = match c {
-                        Command::LedOn => d
-                            .root()
-                            .set_led_state(LedState::On)
-                            .await,
-                        Command::LedOff => d
-                            .root()
-                            .set_led_state(LedState::Off)
-                            .await,
-                        _ => unreachable!(),
-                    };
-                    match r {
-                        Ok(_) => println!("{}", style("ok").green()),
-                        Err(e) => println!("{}", style(e).red()),
-                    }
-                }
-            },
-            ReadCommandOutput::EmptyLine => (),
-            ReadCommandOutput::ClapError(e) => {
-                e.print()?;
-            }
-            ReadCommandOutput::ShlexError => {
-                println!(
-                    "{} input was not valid and could not be processed",
-                    style("Error:").red().bold()
-                );
-            }
-            ReadCommandOutput::ReedlineError(e) => {
-                panic!("{e}");
-            }
-            ReadCommandOutput::CtrlC => continue,
-            ReadCommandOutput::CtrlD => break,
-        }
-    }
-
-    if let Some(d) = driver.as_mut() {
-        d.disconnect_and_exit().await?;
-    }
-    Ok(())
+    rl
 }

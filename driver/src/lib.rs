0962a0d0b187a56d02c5929481abac2d20870d19
Roman Isaikin <romix.lab@gmail.com>
2025-12-09 10:54:50 +0100

    refactor: use connect_raw

--- driver/src/lib.rs
+++ driver/src/lib.rs
@@ -1,47 +1,22 @@
 use std::time::Duration;
-use tokio::sync::mpsc;
 use wire_weaver_client_common::{CommandSender, Error};
-use wire_weaver_usb_host::{usb_worker};
 use wire_weaver::ww_api;
-pub use wire_weaver_client_common::{OnError};
-pub use wire_weaver_client_common::DeviceFilter;
+pub use wire_weaver_client_common::{OnError, DeviceFilter};
 pub use api::LedState;
 
 pub struct MyDeviceDriver {
-    args_scratch: [u8; 512],
+    args_scratch: [u8; 4096],
     cmd_tx: CommandSender,
     timeout: Duration,
 }
 
 impl MyDeviceDriver {
     pub async fn connect(filter: DeviceFilter, on_error: OnError) -> Result<Self, Error> {
-        let (transport_cmd_tx, transport_cmd_rx) = mpsc::unbounded_channel();
-        let (dispatcher_msg_tx, dispatcher_msg_rx) = mpsc::unbounded_channel();
-        let mut cmd_tx = CommandSender::new(transport_cmd_tx, dispatcher_msg_rx);
-        tokio::spawn(async move {
-            usb_worker(transport_cmd_rx, dispatcher_msg_tx, api::DEVICE_API_ROOT_FULL_GID).await;
-        });
-        cmd_tx.connect(filter, api::DEVICE_API_ROOT_FULL_GID.into(), on_error).await?;
-        Ok(Self {
-            args_scratch: [0; 512],
-            cmd_tx,
-            timeout: Duration::from_secs(1),
-        })
+        MyDeviceDriver::connect_raw(filter, api::DEVICE_API_ROOT_FULL_GID, on_error, Duration::from_secs(1), [0u8; 4096]).await
     }
 
     pub fn connect_blocking(filter: DeviceFilter, on_error: OnError) -> Result<Self, Error> {
-        let (transport_cmd_tx, transport_cmd_rx) = mpsc::unbounded_channel();
-        let (dispatcher_msg_tx, dispatcher_msg_rx) = mpsc::unbounded_channel();
-        let mut cmd_tx = CommandSender::new(transport_cmd_tx, dispatcher_msg_rx);
-        tokio::spawn(async move {
-            usb_worker(transport_cmd_rx, dispatcher_msg_tx, api::DEVICE_API_ROOT_FULL_GID).await;
-        });
-        cmd_tx.connect_blocking(filter, api::DEVICE_API_ROOT_FULL_GID.into(), on_error)?;
-        Ok(Self {
-            args_scratch: [0; 512],
-            cmd_tx,
-            timeout: Duration::from_secs(1),
-        })
+        MyDeviceDriver::connect_raw_blocking(filter, api::DEVICE_API_ROOT_FULL_GID, on_error, Duration::from_secs(1), [0u8; 4096])
     }
 }
 
